e0732deef17ade3a908fe22e93b17f3034c61970
ktyu <yu_kitae@naver.com>
2019-02-05 17:42:43 +0900

    10972 - 다음 순열

---
+++ 10972.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int i;
+        for(i=arr.length-1; i-1 >= 0 && arr[i-1] > arr[i]; i--);
+
+        if (i-1 < 0) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        int j = i;
+        while(j < arr.length-1 && arr[i-1] < arr[j+1] && arr[j] > arr[j+1])
+            j++;
+
+
+        int temp = arr[i-1];
+        arr[i-1] = arr[j];
+        arr[j] = temp;
+
+        for(int k=arr.length-1; i<k; i++, k--) {
+            temp = arr[i];
+            arr[i] = arr[k];
+            arr[k] = temp;
+        }
+
+        for(int a : arr)
+            System.out.print(a + " ");
+    }
+}
