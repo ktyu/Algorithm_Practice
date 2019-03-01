5f3e1053ffcbaca6c4f85adeabea3333e9d281b0
ktyu <yu_kitae@naver.com>
2019-03-01 15:21:08 +0900

    2193 - 이친수 (DP)

---
+++ 2193.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long[][] arr = new long[n+1][2];
+
+        arr[1][1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            arr[i][0] = arr[i-1][0] + arr[i-1][1];
+            arr[i][1] = arr[i-1][0];
+        }
+
+        System.out.println(arr[n][0] + arr[n][1]);
+    }
+}
